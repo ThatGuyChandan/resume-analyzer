49852d7fa17955f3494db98d65aee54a0d0abcaf
root <root@LAPTOP-2HI6S5S2>
2025-04-14 13:59:17 +0530

    deploy changes

--- backend/index.js
+++ backend/index.js
@@ -10,19 +10,8 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const allowedOrigins = ["http://localhost:5173", process.env.FRONTEND_URL];
+app.use(cors({ origin: process.env.FRONTEND_URL }));
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-  })
-);
 const upload = multer({ dest: "uploads/" });
 
 const s3 = new S3Client({
