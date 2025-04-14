6e81b2bdebc8750c952fdb76ec7e46a817246cc2
root <root@LAPTOP-2HI6S5S2>
2025-04-14 13:44:31 +0530

    cors fix

--- backend/index.js
+++ backend/index.js
@@ -10,8 +10,19 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-app.use(cors({ origin: "http://localhost:5173" }));
+const allowedOrigins = ["http://localhost:5173", process.env.FRONTEND_URL];
 
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
 const upload = multer({ dest: "uploads/" });
 
 const s3 = new S3Client({
